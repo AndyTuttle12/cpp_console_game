8ee366d4f515d85bf2ba4b883089ccd000338fa0
AndyTuttle12 <andytuttle12@gmail.com>
2018-01-03 22:53:35 -0500

    Added win condition and ended loops.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -32,7 +32,7 @@ int main()
 // Introduce the game
 void PrintIntro()
 {
-	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << "\n\nWelcome to Bulls and Cows, a fun word game.\n";
 	std::cout << "Can you guess the " << BCGame.GetHiddenWordLength();
 	std::cout << " letter isogram I'm thinking of?\n";
 	std::cout << std::endl;
@@ -44,8 +44,9 @@ void PlayGame()
 	BCGame.Reset();
 	int32 MaxTries = BCGame.GetMaxTries();
 
-	// loop for the number of turns asking for guesses
-	for (int32 i = 0; i < MaxTries; i++)
+	// loop asking for guesses while the game is not run
+	// and there are still tries remaining
+	while (!BCGame.IsGameWon() && BCGame.GetCurrentTry() <= MaxTries)
 	{
 		FText Guess = GetValidGuess();
 		
